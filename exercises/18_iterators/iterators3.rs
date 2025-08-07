c1da7ee05792644d8c5b488d8c2e39cf1f32a3e5
Oswald <90507054+Oswald-H@users.noreply.github.com>
2025-08-07 19:56:09 +0800

    Update

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -15,33 +15,27 @@ fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
     // todo!();
     if b == 0 {
         return Err(DivisionError::DivideByZero);
-    }
-
-    if a == i64::MIN && b == -1 {
+    } else if a == i64::MIN && b == -1 {
         return Err(DivisionError::IntegerOverflow);
-    }
-    
-    if a % b != 0 {
+    } else if a % b != 0 {
         return Err(DivisionError::NotDivisible);
+    } else {
+        Ok(a / b)
     }
-
-    Ok(a / b)
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `Ok([1, 11, 1426, 3])`
 fn result_with_list() -> Result<Vec<i64>, DivisionError> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27)).collect();
-    division_results
+    numbers.into_iter().map(|n| divide(*n, 27)).collect()
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
 fn list_of_results() -> Vec<Result<i64,DivisionError>> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27)).collect();
-    division_results
+    numbers.into_iter().map(|n| divide(*n, 27)).collect()
 }
 
 fn main() {
